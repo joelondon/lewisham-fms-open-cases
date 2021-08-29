a07751a813218cec6e5e4d7f673410fb6fe25e25
joelondon <joseph.leach@london.gov.uk>
2021-08-29 14:09:46 +0100

    filter service_requests with jq

--- postprocess.py
+++ postprocess.py
@@ -3,5 +3,5 @@ import json
 
 with open('fixmystreet-open-cases.json','r') as f:
     data = json.loads(f.read())
-    df = pd.json_normalize(data['service_requests'])
+    df = pd.json_normalize(data)
     df.to_csv(r'./fixmystreet-open-cases.csv', index = False, header=True)
